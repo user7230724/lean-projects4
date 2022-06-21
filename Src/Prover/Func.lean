375488758f6614406df85d4a9e056c421796cc10
Patrick Johnson <95079291+user7230724@users.noreply.github.com>
2022-06-21 18:42:53 +0200

    commit

--- Src/Prover/Func.lean
+++ Src/Prover/Func.lean
@@ -15,16 +15,16 @@ someu # λ b => pair a b ∈ f
 
 infixl:100 (priority := high) " ~ " => app
 
-theorem exiu_fun_ret {A : Set} {f : Set → Set} {a : Set} (h : a ∈ A) :
+theorem exiu_fn_ret {A : Set} {f : Set → Set} {a : Set} (h : a ∈ A) :
   ∃! b, pair a b ∈ FN (x ∈ A), f x :=
 exiu_intro (f a) (mpr mem_image # exi_intro a # and_intro h rfl)
 (λ b h₁ => exi_elim (mp mem_image h₁) # λ c h₂ =>
 and_elim (mp pair_ext # and_right h₂) # λ h₃ h₄ =>
 eq_rec (λ x => b = f x) h₃ # eq_symm h₄)
 
-theorem app_fn {A : Set} {f : Set → Set} {a : Set} (h : a ∈ A) :
+theorem fn_app {A : Set} {f : Set → Set} {a : Set} (h : a ∈ A) :
   (FN (x ∈ A), f x) ~ a = f a :=
-hv (@someu_spec _ _ (λ b => pair a b ∈ FN (x ∈ A), f x) # exiu_fun_ret h) #
+hv (@someu_spec _ _ (λ b => pair a b ∈ FN (x ∈ A), f x) # exiu_fn_ret h) #
 λ h₁ => exi_elim (mp mem_image h₁) # λ x h₂ =>
 and_elim (mp pair_ext # and_right h₂) # λ h₃ h₄ =>
 eq_rec (λ x => fn A f ~ a = f x) h₃ # eq_symm h₄
